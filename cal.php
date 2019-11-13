30619d0a9cc2933e4031da7c7c843c2c27431350
KOKI <45584425+naruto1231@users.noreply.github.com>
2019-11-13 18:00:50 +0900

    Add files via upload

---
+++ cal.php
@@ -0,0 +1,60 @@
+<?php
+/*-------------------
+*Class作成
+-------------------*/
+class Cal{
+
+  public $height;
+  public $weight;
+
+  /*
+  * BMI計算関数
+  */
+  function bmi(){
+    #cmをmに変換
+    $height =$this->height/100;
+    #BMI計算
+    $bmi = $this->weight/($height * $height);
+    #小数点以下を四捨五入
+    $bmi =round($bmi);
+    return"$bmi";
+  }
+  /*
+  *BMI判定関数
+  */
+  function hentai(){
+    #cmをmに変換
+    $height = $this->height/100;
+    #BMI計算
+    $bmi = $this->weight/($height * $height);
+    #小数点以下を四捨五入
+    $bmi = round($bmi);
+
+    BMi判定
+    if($bmi < 18.5){
+      $result = "低体重(やせ)";
+    }else if($bmi < 25){
+      $result = "標準";
+    }else if($bmi < 30){
+      $result = "肥満度1度"
+    }
+    else if($bmi < 35){
+      $result = "肥満2度";
+    }
+    else if($bmi < 40){
+      $result = "肥満3度";
+    }
+    else if($bmi >= 40){
+      $result = "肥満4度";
+    }
+    return"$result";
+  }
+}
+/*----------------------------
+インスタンス生成
+----------------------------*/
+$cal = new Cal();
+$cal->height = htmlspecialchars($_POST['height']);
+$cal->weight = htmlspecialchars($_POST['weight']);
+
+?>
---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="device-width", initial-scale=1>
+<title>BMI計算</title>
+<!---
+form
+--->
+<form action="cal.php" method="post" role="form">
+  <div class="form-group">
+  <label for="InputHeight">身長(cm)</lavel>
+  <input type="text" name="height" class="form-control" id="InputHeight" placeholder="半角数字のみ">
+  <input type="text" name="height" class="form-control" id="InputHeight" placeholder="半角数字のみ">
+</div>
+<div class="form-group">
+<lavel for"Inputeweight">体重(kg)</label>
+<input type="text" name="weight" class="form-control" id="Inputweight" placeholder="半角数字のみ">
+</div>
+<button type="submit" class="btn-default">BMI計算</button>
+</form>
+</body>
+</html>
